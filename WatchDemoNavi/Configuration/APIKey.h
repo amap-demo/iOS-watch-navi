1efc76d77364ca01ca1f67ae0b45b90635ab5915
liubo.liub <liubo.liub@alibaba-inc.com>
2016-12-19 16:56:18 +0800

    add key

--- WatchDemoNavi/Configuration/APIKey.h
+++ WatchDemoNavi/Configuration/APIKey.h
@@ -11,6 +11,6 @@
 
 /* 使用高德地图API，请注册Key，注册地址：http://lbs.amap.com/console/key */
 
-const static NSString *APIKey = @"";
+const static NSString *APIKey = @"7f0c1047872ebc023547043c18f4aa83";
 
 #endif
